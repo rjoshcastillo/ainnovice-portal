834535dd20e13f00d006f885a7696c625a9eea6d
Joshua Ramos <jramos@yondu.com>
2024-10-22 00:32:45 +0800

    malinis commit

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import AInnoviceLogo from "../assets/logo/ainnovice_logo.png";
 import { useTheme } from "@mui/material/styles";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { useUser } from "../context/UserContext";
 
 function AppNavBar() {
@@ -13,17 +14,22 @@ function AppNavBar() {
   };
   const handleHomeClick = () => {
     navigate("/");
-  }
+  };
   return (
     <AppBar
       position="static"
       sx={{ backgroundColor: theme.palette.primary.main }}
     >
       <Toolbar>
-      <img
+        <img
           src={AInnoviceLogo}
           alt="Logo"
-          style={{ width: "75px", height: "75px", marginRight: "10px", cursor: "pointer" }}
+          style={{
+            width: "75px",
+            height: "75px",
+            marginRight: "10px",
+            cursor: "pointer",
+          }}
           onClick={handleHomeClick}
         />
         <Typography
@@ -34,21 +40,25 @@ function AppNavBar() {
         >
           AInnovice
         </Typography>
-        <Button color="inherit" onClick={handleHomeClick} >Home</Button>
-        <Button color="inherit">Our Services</Button>
-        <Button color="inherit">About Us</Button>
-        {!user.isLogin && (
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: theme.palette.tertiary.main,
-              color: theme.palette.primary.main,
-            }}
-            onClick={handleLoginClick}
-          >
-            Login
+        <Grid sx={{ display: 'flex', gap: 4 }}>
+          <Button color="inherit" onClick={handleHomeClick}>
+            Home
           </Button>
-        )}
+          <Button color="inherit">Our Services</Button>
+          <Button color="inherit">About Us</Button>
+          {!user.isLogin && (
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: theme.palette.tertiary.main,
+                color: theme.palette.primary.main,
+              }}
+              onClick={handleLoginClick}
+            >
+              Login
+            </Button>
+          )}
+        </Grid>
       </Toolbar>
     </AppBar>
   );
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -13,10 +13,10 @@ import HeaderImage from "../../assets/doctor-patient-image.svg";
 import HospitalService from "../../assets/hospital-service.svg";
 import PatientSupport from "../../assets/patient-support.svg";
 import DoctorConsultation from "../../assets/doctor-consultation.svg";
-import { useUser } from "../../context/UserContext"; 
+import { useUser } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
-  const { user } = useUser(); 
+  const { user } = useUser();
   const navigate = useNavigate(); // For navigation
 
   const handleAppointmentClick = () => {
@@ -27,55 +27,69 @@ const Dashboard = () => {
     }
   };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Container sx={{ my: 4 }}>
-        <Grid container spacing={4} alignItems="center">
+    <Box>
+      <Container sx={{ my: 6 }}>
+        <Card sx={{ padding: 4 }}>
           <Grid item xs={12} md={6}>
             <Typography variant="h4" component="h1" gutterBottom>
               Welcome to our Online Services
             </Typography>
             <Typography variant="body1" paragraph>
-              Here at Chong Hua Hospital, we offer the most comprehensive online
-              healthcare services at affordable costs.
+              At <strong>AINNovice</strong>, we are dedicated to revolutionizing your healthcare
+              experience with our AI-powered online services, tailored to meet
+              all your needs. Your health is our top
+              priority!
             </Typography>
-            <Button variant="contained" color="primary" onClick={handleAppointmentClick}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleAppointmentClick}
+              style={{ padding: 12 }}
+            >
               Book an Appointment
             </Button>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <img
-              src={HeaderImage}
-              alt="Online Services"
-              style={{ width: "100%" }}
-            />
-          </Grid>
-        </Grid>
+        </Card>
       </Container>
-
-      {/* Services Section */}
-      <Container sx={{ py: 4 }}>
-        <Grid container spacing={4}>
-          {[
-            { title: "Hospital Services", imgSrc: HospitalService },
-            { title: "Doctor Consultation", imgSrc: DoctorConsultation },
-            { title: "Patient Support", imgSrc: PatientSupport },
-          ].map((service) => (
-            <Grid item xs={12} sm={6} md={4} key={service.title}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  height="170"
-                  image={service.imgSrc}
-                  alt={service.title}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {service.title}
-                  </Typography>
-                </CardContent>
-              </Card>
+      <Container>
+        <Grid container sx={{ justifyContent: "space-between"}}>
+          <Grid size={5} sx={{ my: 12}}>
+            <Grid item>
+              <img
+                src={HeaderImage}
+                alt="Online Services"
+                style={{ width: "100%" }}
+              />
             </Grid>
-          ))}
+          </Grid>
+          <Grid size={6}>
+            {/* Services Section */}
+            <Container sx={{ padding: 4, marginLeft: 5 }}>
+              <Grid container spacing={4} sx={{ cursor: "pointer" }}>
+                {[
+                  { title: "Hospital Services", imgSrc: HospitalService },
+                  { title: "Doctor Consultation", imgSrc: DoctorConsultation },
+                  { title: "Patient Support", imgSrc: PatientSupport },
+                ].map((service) => (
+                  <Grid item xs={12} sm={6} md={4} key={service.title}>
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        height="170"
+                        image={service.imgSrc}
+                        alt={service.title}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {service.title}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          </Grid>
         </Grid>
       </Container>
     </Box>
