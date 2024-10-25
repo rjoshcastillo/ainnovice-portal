b0520f58323116709d5f7dbd8c31c25ba8d07ae8
Joshua Ramos <jramos@yondu.com>
2024-10-25 13:50:43 +0800

    api endpoint

--- src/services/endpoint.js
+++ src/services/endpoint.js
@@ -1,5 +1,5 @@
 // export const baseApi = 'http://localhost:5000/api'
-export const baseApi = `http://localhost:5000/api`
+export const baseApi = `https://0900-14-1-92-87.ngrok-free.app/api`
 export const Account = `${baseApi}/account`;
 export const Login = `${Account}/login`
 export const doctor = `${baseApi}/doctor`;
